accb8f6ebc8b3cf7679183eb5e545b056fd90920
Reuven <retzion@gmail.com>
2020-12-10 23:01:21 -0700

    (fix) Copy link function uses new seo proxy server

--- src/Views/Map/SelectedPlaceSideBar/SidebarControls/index.js
+++ src/Views/Map/SelectedPlaceSideBar/SidebarControls/index.js
@@ -34,7 +34,7 @@ export default function ({
   const { name, reference, vicinity } = selected || {}
 
   // const baseUrl = `${document.location.protocol}//${document.location.host}/location/${reference}?details`
-  const baseUrl = encodeURIComponent(`http://link.maskforecast.com/${reference}`)
+  const baseUrl = `http://link.maskforecast.com/${reference}`
   const [showShareOptions, setShowShareOptions] = useState(null)
   const [clipboard, setClipboard] = useState({
     value: baseUrl,
